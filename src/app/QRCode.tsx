aec4d0a00cbba5e71d1768a9efeead24d1c1be06
zharkyn <kzh.light@gmail.com>
2023-11-02 12:56:22 -0700

    added browserlint support ie 10

--- src/app/QRCode.tsx
+++ src/app/QRCode.tsx
@@ -37,7 +37,7 @@ export function QRCode() {
   return (
     <>
       <div ref={ref} />
-      {/* <ReactQRCode
+      <ReactQRCode
         value="lightning:LNTB3640N1PJ3A773PP5U09UPQ8SQG6JDVCSCDAY9PFJXAMF7XP3Z2ER07QT2VSZLHUCD8LSDPZ09PXVVPJGVUNJ5RWYQKJQSTDV9K9X6R0WQCQZZSXQZJHSP5R7GUW3PGUPHUFFPSDVPY7LJVEFV5TGLJ80T4YRVD9CQ4UDE7QEXS9QYYSSQZLP32K4FKY0WR8P9JHFZK5569GA4YFZGF4VV0XL6FP5EP9ZU3T445DVJWGNAHYNSS5M77D6J82AUQDTZ55TKVEGVFGJNC8QE7VET8QCPE3THEU"
         size={220}
         qrStyle="dots"
@@ -52,7 +52,7 @@ export function QRCode() {
         logoPadding={4}
         removeQrCodeBehindLogo={true}
         logoPaddingStyle="circle"
-      /> */}
+      />
     </>
   )
 }
