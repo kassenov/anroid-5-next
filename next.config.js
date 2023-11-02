a2fbaf51b35003a7c87c33190712907929b1e25a
zharkyn <kzh.light@gmail.com>
2023-11-02 13:27:37 -0700

    disabel strict mode

--- next.config.js
+++ next.config.js
@@ -4,14 +4,15 @@ const nextConfig = {
         "react-qrcode-logo",
         "qr-code-styling-new"
     ],
+    reactStrictMode: false,
     webpack: (
         config,
         { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
     ) => {
-    config.optimization.minimize = false;
-    config.optimization.minimizer = [];
-    // Important: return the modified config
-    return config
+        config.optimization.minimize = false;
+        config.optimization.minimizer = [];
+        // Important: return the modified config
+        return config
     },
 }
 
