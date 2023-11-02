147835eb37a437dd63d8c5cc57f1ed5a291e9564
zharkyn <kzh.light@gmail.com>
2023-11-02 13:19:48 -0700

    disable minimization

--- next.config.js
+++ next.config.js
@@ -3,7 +3,15 @@ const nextConfig = {
     transpilePackages: [
         "react-qrcode-logo",
         "qr-code-styling-new"
-    ]
+    ],
+    webpack: (
+        config,
+        { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack }
+    ) => {
+    config.optimization.minimize = false;
+    // Important: return the modified config
+    return config
+    },
 }
 
 module.exports = nextConfig
