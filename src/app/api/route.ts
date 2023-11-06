c0e1b93a218e19675bdd723a6d5571a7045f0245
zharkyn <kzh.light@gmail.com>
2023-11-06 11:08:21 -0800

    redirect test

---
+++ src/app/api/route.ts
@@ -0,0 +1,14 @@
+import { redirect } from 'next/navigation';
+ 
+export async function GET(request: Request) {
+    // Parse the user agent from the request headers
+    const userAgent = request.headers.get('user-agent');
+    // Check if the user agent contains "Chrome/39" or lower
+    const isChrome39OrLower = /Chrome\/(?:0[0-9]|1[0-9]|2[0-9]|3[0-9])/.test(userAgent || "");
+
+    if (isChrome39OrLower) {
+      // Redirect to a specific page for older Chrome versions
+      redirect('https://www.google.com/');
+    } else {
+    }
+}
