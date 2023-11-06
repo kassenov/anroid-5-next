ab2aae28ab916ed54c985475e6e79161e5392130
zharkyn <kzh.light@gmail.com>
2023-11-06 15:16:47 -0800

    fixes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -26,7 +26,6 @@ export default function Home() {
 
   const toggle = () => setModal(!modal);
 
-  const connect = () => {
     const socket = io("https://9781-2604-3d08-9d86-4700-6513-f248-64a0-111d.ngrok-free.app", {
       autoConnect: false,
       auth: {
@@ -35,7 +34,6 @@ export default function Home() {
     })
 
     socket.connect()
-  }
 
   return (
     <main>
@@ -134,9 +132,6 @@ export default function Home() {
     </div>
   </div>
 </div> */}
-
-    <Script src="/socket.io/socket.io.js"></Script>
-    <Script onLoad={() => connect()}/>
 
     </main>
   )
